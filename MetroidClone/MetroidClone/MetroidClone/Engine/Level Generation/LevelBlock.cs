ba319b0d24376c0f02705d569c75fa31419e1920
Florian van Strien <flori9@live.nl>
2015-12-06 14:16:28 +0100

    More level generation things

--- MetroidClone/MetroidClone/MetroidClone/Engine/Level Generation/LevelBlock.cs
+++ MetroidClone/MetroidClone/MetroidClone/Engine/Level Generation/LevelBlock.cs
@@ -7,6 +7,92 @@ namespace MetroidClone.Engine
 {
     class LevelBlock
     {
+        public char[,] Data;
+        public readonly int Width, Height;
 
+        public bool HasLeftExit, HasRightExit, HasUpExit, HasDownExit;
+        public bool HasLeftWall, HasRightWall, HasUpWall, HasDownWall;
+
+        public LevelBlock()
+        {
+            Width = 5;
+            Height = 5;
+
+            Data = new char[Width, Height];
+
+            HasLeftExit = false;
+            HasRightExit = false;
+            HasUpExit = false;
+            HasDownExit = false;
+
+            HasLeftWall = false;
+            HasRightWall = false;
+            HasUpWall = false;
+            HasDownWall = false;
+        }
+
+        //Updates information about level wall etc.
+        public void UpdateLevelInfo()
+        {
+            char[] wallChars = {'1', '2'}; //Characters that count as a wall.
+
+            //At the beginning, assume that there's a wall on each side.
+            HasLeftWall = true;
+            HasRightWall = true;
+            HasUpWall = true;
+            HasDownWall = true;
+
+            //Then, check if that's true.
+            for (int i = 0; i < 5; i++)
+            {
+                if (!wallChars.Contains(Data[0, i]))
+                    HasLeftWall = false;
+                if (!wallChars.Contains(Data[4, i]))
+                    HasRightWall = false;
+                if (! wallChars.Contains(Data[i, 0]))
+                    HasUpWall = false;
+                if (! wallChars.Contains(Data[i, 4]))
+                    HasDownWall = false;
+            }
+        }
+
+        //Place a level block in a level. The preferred walls can be specified. These can only be guaranteed
+        // if the level has the wall type (for example, preferLeftWall can be ignored if HasLeftWall is false).
+        //If a wall isn't preferred at a position, an exit is used instead if Has*Exit is true.
+        public void Place(Level level, int x, int y, bool preferLeftWall = true, bool preferRightWall = true,
+            bool preferUpWall = true, bool preferDownWall = true)
+        {
+            for (int i = x; i < x + Width; i++)
+            {
+                for (int j = y; j < y + Height; j++)
+                {
+                    if (Data[i, j] == '1') //A wall
+                        level.Grid[i, j] = true;
+                    else if (Data[i, j] == '2') //Might be a wall
+                    {
+                        if (i == x) //Left wall
+                        {
+                            if (preferLeftWall)
+                                level.Grid[i, j] = true;
+                        }
+                        if (i == x + Width - 1) //Right wall
+                        {
+                            if (preferRightWall)
+                                level.Grid[i, j] = true;
+                        }
+                        if (j == y) //Top wall
+                        {
+                            if (preferUpWall)
+                                level.Grid[i, j] = true;
+                        }
+                        if (j == y + Height - 1) //Bottom wall
+                        {
+                            if (preferDownWall)
+                                level.Grid[i, j] = true;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- MetroidClone/MetroidClone/MetroidClone/Engine/Level Generation/LevelGenerator.cs
+++ MetroidClone/MetroidClone/MetroidClone/Engine/Level Generation/LevelGenerator.cs
@@ -8,13 +8,18 @@ namespace MetroidClone.Engine
 {
     class LevelGenerator
     {
-        Level Level;
-        int Width, Height;
+        List<LevelBlock> levelBlocks;
 
+        public Level Level { get; protected set; }
+        public readonly int Width, Height;
+
+        //The width and height of the level can be given when creating a level.
         public LevelGenerator()
         {
             Width = 20;
             Height = 15;
+
+            levelBlocks = new List<LevelBlock>();
 
             GetLevelBlocks();
         }
@@ -23,12 +28,8 @@ namespace MetroidClone.Engine
         {
             Level = new Level();
 
-            bool[,] grid = new bool[Width, Height];
+            Level.Grid = new bool[Width, Height]; //Clear the level
 
-            for (int i = 0; i < Width; i++)
-                grid[i, Height - 1] = true;
-
-            Level.Grid = grid;
 
             return Level;
         }
@@ -37,16 +38,61 @@ namespace MetroidClone.Engine
         void GetLevelBlocks()
         {
             string levelBlocksData = File.ReadAllText("Content/LevelBlocks.txt");
+            //Handle the different types of enter (\r\n, \r, and \n)
+            levelBlocksData = levelBlocksData.Replace("\r\n", "\n");
+            levelBlocksData = levelBlocksData.Replace("\r", "\n");
             string[] levelBlocksLines = levelBlocksData.Split('\n');
+            int thisBlockDataHeight = 0; //The current height of a level block.
 
-            LevelBlock
+            LevelBlock currentLevelBlock = null;
 
             for (int i = 0; i < levelBlocksLines.Length; i++)
             {
                 string line = levelBlocksLines[i];
                 if (line.StartsWith("BLOCK"))
                 {
+                    currentLevelBlock = new LevelBlock();
+                    thisBlockDataHeight = 0;
+                    levelBlocks.Add(currentLevelBlock);
 
+                    line = line.Replace("BLOCK", ""); //Delete the BLOCK part of the line.
+                    //If there's a space at the beginning now, remove that, too.
+                    if (line.StartsWith(" "))
+                        line = line.Remove(0, 1);
+
+                    //Now check the arguments of the block.
+                    string[] arguments = line.Split(' ');
+
+                    //The first argument should contain information about the exits of the block.
+                    string exits = arguments[0];
+
+                    if (exits.Contains("U"))
+                        currentLevelBlock.HasUpExit = true;
+                    if (exits.Contains("D"))
+                        currentLevelBlock.HasDownExit = true;
+                    if (exits.Contains("L"))
+                        currentLevelBlock.HasLeftExit = true;
+                    if (exits.Contains("R"))
+                        currentLevelBlock.HasRightExit = true;
+
+                    //TODO: the second argument can now contain information about difficulty etc.
+                }
+                else if (currentLevelBlock != null)
+                {
+                    if (! string.IsNullOrWhiteSpace(line)) //Set the level data.
+                    {
+                        for (int j = 0; j < line.Length; j++)
+                        {
+                            currentLevelBlock.Data[j, thisBlockDataHeight] = line[j];
+                        }
+                        thisBlockDataHeight++;
+                    }
+                    else //An empty line means the end of the block definition of this block.
+                    {
+                        currentLevelBlock.UpdateLevelInfo();
+
+                        currentLevelBlock = null;
+                    }
                 }
             }
         }
